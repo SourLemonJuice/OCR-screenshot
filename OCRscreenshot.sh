dc500042e1d87af612dd24857d566266cd1c1d38
SourLemonJuice <SourLemonJuice233@outlook.com>
2024-03-08 18:17:12 +0800

    添加复制翻译内容功能
    同时也修整了代码逻辑和提示输出

--- OCRscreenshot.sh
+++ OCRscreenshot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# OCRscreenshot.sh
 
 # 使用 tesseract 识别屏幕截图上的文字
 
@@ -16,31 +17,55 @@ _Tesseract_Configs+="-c preserve_interword_spaces=1"
 # 语言
 _Translate_Shell_Lang="zh"
 
+function OCRscreenshot_getText
 {
     # 启动截图，输出至标准输出 [--后台 --无通知 --自定义输出路径到标准输出]
-    spectacle "$_Capture_Mode" --background --nonotify --output "/proc/self/fd/1"
-    # 输出图像
-} | {
-    tesseract stdin stdout -l "$_Tesseract_Lang" --oem 3 --psm 6 ${_Tesseract_Configs[@]}
-    # 输出解析后的文字
-} | {
-    _OUT_TEXT="$(cat)"
-    # 发出通知
-    echo "OCR: $_OUT_TEXT"
-    _NOTIFY_ACTION=$(notify-send --app-name OCR --action Copy=复制 --action Translate=翻译 "$_OUT_TEXT")
-    echo "notify action is: $_NOTIFY_ACTION"
-    # 实现控件功能
-    case "$_NOTIFY_ACTION" in
-    Copy)
-        wl-copy "$_OUT_TEXT"
-        echo "wl-copy: $_OUT_TEXT"
-        ;;
-    Translate)
-        _TRANS_OUT=$(trans -b -t "$_Translate_Shell_Lang" "$_OUT_TEXT")
-        echo "translate out: $_TRANS_OUT"
-        notify-send --app-name OCR_translate "$_TRANS_OUT"
-        ;;
-    esac
+    spectacle "$_Capture_Mode" --background --nonotify --output "/proc/self/fd/1" |
+    # 调用 tesseract
+    tesseract stdin stdout -l "$_Tesseract_Lang" --oem 3 --psm 6 ${_Tesseract_Configs[@]} |
+    # 输出解析后的文字到标准输出
+    cat
 }
-# done
-exit 0
+
+function OCRscreenshot_main
+{
+    OCRscreenshot_getText |
+    {
+        # 从管道中获取文字
+        local _OUT_TEXT="$(cat)"
+        echo "OCR: $_OUT_TEXT"
+        # 发出通知
+        local OCR_Notify_Action=$(notify-send --app-name OCR --action ocr_Copy=复制 --action ocr_Translate=翻译 "$_OUT_TEXT")
+        echo "notify action is: '$OCR_Notify_Action'"
+        # 实现
+        case "$OCR_Notify_Action" in
+        ocr_Copy)
+            wl-copy "$_OUT_TEXT"
+            echo "wl-copy: $_OUT_TEXT"
+            ;;
+        ocr_Translate)
+            # 获取翻译结果
+            local _TRANS_OUT=$(trans -b -t "$_Translate_Shell_Lang" "$_OUT_TEXT")
+            echo "translate out: $_TRANS_OUT"
+            # 发送通知
+            local OCR_trans_Notify_Action=$(notify-send --app-name OCR_translate --action trans_Copy=复制 "$_TRANS_OUT")
+            echo "OCR.trans action is: $OCR_trans_Notify_Action"
+            # 实现通知操作
+            case "$OCR_trans_Notify_Action" in
+            trans_Copy)
+                wl-copy "$_TRANS_OUT"
+                echo "wl-copy: $_TRANS_OUT"
+                ;;
+            *)
+                echo "OCR.trans: no trans notify actions were perform"
+            ;;
+            esac
+            ;;
+        *)
+            echo "OCR: no notify actions were perform"
+            ;;
+        esac
+    }
+}
+# main function
+OCRscreenshot_main
