08fddd9463d44d44f9b9c6db95021868198df8db
SourLemonJuice <SourLemonJuice233@outlook.com>
2024-04-16 00:18:55 +0800

    添加终端界面模式

--- OCRscreenshot.sh
+++ OCRscreenshot.sh
@@ -3,6 +3,8 @@
 
 # 使用 tesseract 识别屏幕截图上的文字
 
+# ==== configs ====
+
 # spectacle 矩形模式
 _Capture_Mode="--region"
 
@@ -17,6 +19,48 @@ _Tesseract_Configs+="-c preserve_interword_spaces=1"
 # 语言
 _Translate_Shell_Lang="zh"
 
+# 主程序设置
+# 结果输出模式
+Result_Mode="Notification"
+
+# ==== librarys ====
+
+# clipboard v2
+# dependency:
+# - wl-clipboard
+# - xclip
+
+llib_clipboard_copy() {
+    # 检测正在使用的桌面gui程序，并选择对应的剪贴板工具
+    case $XDG_SESSION_TYPE in
+    wayland)
+        # wl-clipboard 是一个wayland下的剪贴板工具集
+        wl-copy "$1"
+        ;;
+    x11)
+        # 那这个就是老古董x11的喽
+        # xclip 的实现
+        echo "$1" | xclip -selection c
+        # xsel 的实现
+        # echo "$Parsed_Code" | xsel -ib
+        ;;
+    esac
+}
+
+llib_clipboard_paste() {
+    case $XDG_SESSION_TYPE in
+    wayland)
+        wl-paste
+        ;;
+    x11)
+        echo "只找了wayland模式的工具嘞"
+        ;;
+    esac
+}
+
+# ==== END librarys ====
+
+# Get text to stdout from "spectacle"
 function OCRscreenshot_getText
 {
     # 启动截图，输出至标准输出 [--后台 --无通知 --自定义输出路径到标准输出]
@@ -27,21 +71,21 @@ function OCRscreenshot_getText
     cat
 }
 
-function OCRscreenshot_main
+# Final return result using multi-way
+# - Notification
+# - Terminal
+function Return_result
 {
-    OCRscreenshot_getText |
-    {
-        # 从管道中获取文字
-        local _OUT_TEXT="$(cat)"
-        echo "OCR: '$_OUT_TEXT'"
+    case "$1" in
+    Notification)
         # 发出通知
         local OCR_Notify_Action=$(notify-send --app-name OCR --action ocr_Copy=复制 --action ocr_Translate=翻译 "$_OUT_TEXT")
         echo "notify action is: '$OCR_Notify_Action'"
         # 实现
         case "$OCR_Notify_Action" in
         ocr_Copy)
-            wl-copy "$_OUT_TEXT"
-            echo "wl-copy: $_OUT_TEXT"
+            llib_clipboard_copy "$_OUT_TEXT"
+            echo "clipboard-copy: $_OUT_TEXT"
             ;;
         ocr_Translate)
             # 获取翻译结果
@@ -53,8 +97,8 @@ function OCRscreenshot_main
             # 实现通知操作
             case "$OCR_trans_Notify_Action" in
             trans_Copy)
-                wl-copy "$_TRANS_OUT"
-                echo "wl-copy: '$_TRANS_OUT'"
+                llib_clipboard_copy "$_TRANS_OUT"
+                echo "clipboard-copy: '$_TRANS_OUT'"
                 ;;
             *)
                 echo "OCR.trans: no trans notify actions were perform"
@@ -65,7 +109,58 @@ function OCRscreenshot_main
             echo "OCR: no notify actions were perform"
             ;;
         esac
+        ;;
+
+    Terminal)
+        # Show out text
+        # by echo
+        # echo "$_OUT_TEXT"
+        # by "gum pager"
+        gum pager "$_OUT_TEXT"
+
+        # Choose what to do
+        case $(gum choose "Copy" "Translate") in
+        Copy)
+            # Copy it with lib
+            llib_clipboard_copy "$_OUT_TEXT"
+            ;;
+        Translate)
+            local Translate_Output=$(trans -b -t "$_Translate_Shell_Lang" "$_OUT_TEXT")
+            gum pager "$Translate_Output"
+            # Ask if copying
+            case $(gum choose "Copy") in
+            Copy)
+                llib_clipboard_copy "$Translate_Output"
+                ;;
+            *)
+                return 1
+                ;;
+            esac
+            # END Translate
+            ;;
+        esac
+        # END Terminal mode action
+        ;;
+    esac
+    # END Return_result
+}
+
+function OCRscreenshot_main
+{
+    OCRscreenshot_getText |
+    {
+        # 从管道中获取文字
+        local _OUT_TEXT="$(cat)"
+        echo "OCR: '$_OUT_TEXT'"
+        case "$Result_Mode" in
+        Notification)
+            Return_result "Notification"
+            ;;
+        Terminal)
+            Return_result "Terminal"
+            ;;
+        esac
     }
 }
-# main function
+# Run main function
 OCRscreenshot_main
--- README.md
+++ README.md
@@ -1,27 +1,54 @@
 # OCR-screenshot
 
-## 1.简介/使用
+## 1.简介与基本使用
 
-一个用来快速翻译屏幕上文字的小脚本
+一个用来快速翻译屏幕上文字的 bash 脚本
+
+### 使用通知模式 (Notification)
 
 - 调用`spectacle`截图
 - `tesseract`OCR识别，并发出带控件的通知
 - 如果选择**复制**用`wl-copy(wl-clipboard)`复制
 - 选择**翻译**用`Translate Shell`翻译，并通知(可以复制译文)
 
-这也就是全部的依赖项了\
-这是 archlinux 软件包名列表:
+绑定快捷键时可以直接选择 `.sh` 脚本文件\
+当然 `Result_Mode` 还是需要被配置成 Notification 的
+
+### 使用终端模式 (Terminal)
+
+既然是截屏翻译，那在截屏之前显示个终端出来挡着是不是有点傻...\
+但反正有这个模式嘛
+
+基本就是用 `gum` 重新实现的通知模式，gum 这东西超棒的（虽然我测试时边框有点bug）
+
+拥有更大的文字展示空间以及美观询问界面
+
+可以用 `OCRscreenshot-Terminal.desktop` 作为快捷键的目标，或者能让它在终端里运行就可以\
+注意记得更改 `Exec=` 条目的**绝对路径**，desktop 文件只能用绝对路径嘛
+
+同样的 `Result_Mode` 需要被配置成 Terminal
+
+## 依赖项目列表
+
+这就是全部的依赖项了\
+这是 Archlinux 软件包名列表:
 
 - spectacle
 - tesseract
-- wl-clipboard
+- wl-clipboard (wayland)
+- xclip (x11)
 - translate-shell
+- gum (terminal mode)
 
-## 2.一些配置
+```text
+spectacle tesseract wl-clipboard xclip translate-shell gum
+```
+
+## 2.一些重要配置
 
 ### 2.1.Tesseract
 
-```shell
+```bash
 # Tesseract 设置
 # 语言
 _Tesseract_Lang="eng+chi_sim"
@@ -32,10 +59,17 @@ _Tesseract_Configs+="-c preserve_interword_spaces=1"
 
 ### 2.2.Translate Shell
 
-```shell
+```bash
 # Translate Shell 设置 
 # 语言
 _Translate_Shell_Lang="zh"
+```
+
+### 2.3.主程序设置
+
+```bash
+# 结果输出模式
+Result_Mode="Notification"
 ```
 
 ## --.碎碎念
@@ -46,3 +80,7 @@ _Translate_Shell_Lang="zh"
 总之写了个小东西玩，平时用的估计也不多啦，都是高强度看网页\
 而且这东西还不能接受`--/-`开头的输入，会被当成选项用然后通知就报错了，咕\
 shell脚本好神奇，我一定要去学编程语言了，aaa
+
+### --.TODO
+
+- 至于内容误识别成选项（-- 开头）这种事嘛，要不换成高端一点的编译语言或者脚本语言
